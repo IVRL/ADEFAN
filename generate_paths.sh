4c0bdc79bf1baa6d22863eef958af2a57ebd4654
Frank Schmutz <frank.schmutz@epfl.ch>
2019-11-04 18:24:56 +0100

    fixed generate_paths script for Linux

--- generate_paths.sh
+++ generate_paths.sh
@@ -5,8 +5,8 @@ mkdir -p ../Results/Grids
 
 for i in 1 2 3 4 5 6 7 8
 do
-  mkdir -p ../Results/CompressedVideos/$ipercent
-  mkdir -p ../Results/ReconstructedVideos/$ipercent/EFAN2D
-  mkdir -p ../Results/ReconstructedVideos/$ipercent/EFAN3D
-  mkdir -p ../Results/ReconstructedVideos/$ipercent/ADEFAN
+  mkdir -p ../Results/CompressedVideos/$i\percent
+  mkdir -p ../Results/ReconstructedVideos/$i\percent/EFAN2D
+  mkdir -p ../Results/ReconstructedVideos/$i\percent/EFAN3D
+  mkdir -p ../Results/ReconstructedVideos/$i\percent/ADEFAN
 done
